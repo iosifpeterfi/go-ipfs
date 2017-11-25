bb2cac9dcc3edc9aa566c0bd9eb18fef79d18fc9
root <root@db01.schnellno.de>
2017-11-25 10:39:20 +0100

    * Changed the github repo path in the make files

--- mk/golang.mk
+++ mk/golang.mk
@@ -11,10 +11,10 @@ DEPS_GO :=
 TEST_GO :=
 CHECK_GO :=
 
-go-pkg-name=$(shell $(GOCC) list $(go-tags) github.com/ipfs/go-ipfs/$(1))
+go-pkg-name=$(shell $(GOCC) list $(go-tags) github.com/iosifpeterfi/go-ipfs/$(1))
 go-main-name=$(notdir $(call go-pkg-name,$(1)))$(?exe)
 go-curr-pkg-tgt=$(d)/$(call go-main-name,$(d))
-go-pkgs-novendor=$(shell $(GOCC) list github.com/ipfs/go-ipfs/... | grep -v /Godeps/)
+go-pkgs-novendor=$(shell $(GOCC) list github.com/iosifpeterfi/go-ipfs/... | grep -v /Godeps/)
 
 go-tags=$(if $(GOTAGS), -tags="$(call join-with,$(space),$(GOTAGS))")
 go-flags-with-tags=$(GOFLAGS)$(go-tags)
@@ -54,7 +54,7 @@ check_go_version:
 DEPS_GO += check_go_version
 
 check_go_path:
-	bin/check_go_path $(realpath $(shell pwd)) $(realpath $(addsuffix /src/github.com/ipfs/go-ipfs,$(subst $(PATH_SEP),$(space),$(GOPATH))))
+	bin/check_go_path $(realpath $(shell pwd)) $(realpath $(addsuffix /src/github.com/iosifpeterfi/go-ipfs,$(subst $(PATH_SEP),$(space),$(GOPATH))))
 .PHONY: check_go_path
 DEPS_GO += check_go_path
 
